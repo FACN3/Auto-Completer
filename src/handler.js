87b8f4f164dbe94bc9b196a54abf4f6f858d4022
MynahMarie <mynah.marie@gmail.com>
2017-11-21 17:44:54 +0200

    Start adding routes to handler.js
    Relates #13

--- src/handler.js
+++ src/handler.js
@@ -1,7 +1,11 @@
 var fs = require('fs');
 var path = require('path');
-
 var querystring = require('querystring');
+var filepath;
+
+function handleError(err) {
+
+}
 
 function handler(req, res) {
 
@@ -9,19 +13,58 @@ function handler(req, res) {
   console.log(url);
 
   if (url === '/') {
-    //display index.html
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write("This is HOME");
-    res.end();
+    //Display index.html
+    filepath = path.join(__dirname, '..', 'public', 'index.html');
+
+    fs.readFile(filepath, function(error, file) {
+      if (error) {
+        console.log(error);
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<h1>FiLE NoT FouNd</h1>');
+      } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(file);
+      }
+    });
   }
   else if (url === '/search') {
-    // filter and load data into page
+    var data = [];
+    filepath = path.join(__dirname, '..', 'small.txt');
+    fs.readFile(filepath, function(error, file) {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      data = filepath.split('\n');
+      console.log(data);
+      res.end(file);
+    });
   }
   else {
-    // load assets
-    res.writeHead(404, {'Content-Type': 'text/plain'});
-    res.write("File not found.");
-    res.end();
+    // Load assets
+    ext = url.split('.')[1];
+    console.log(ext);
+
+    var extTypes = {
+      css: 'text/css',
+      js: 'application/javascript',
+      html: 'text/html',
+      png: 'image/png'
+    }
+
+    filepath = path.join(__dirname, '..', 'public', url);
+
+    fs.readFile(filepath, function(error, file) {
+      if (error) {
+        console.log(error);
+        res.writeHead(404, {'Content-Type': 'text/plain'});
+        res.end('<h1>FiLE NoT FouNd</h1>');
+        res.end();
+      } else {
+        res.writeHead(200, {'Content-Type': extTypes[ext]});
+        res.end(file);
+      }
+    });
   }
 }
 
