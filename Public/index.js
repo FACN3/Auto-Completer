a6955cc5726732be3a5145dff9ac75ad190f7622
SamiSha99 <sami.shakkour@hotmail.com>
2017-11-21 17:41:01 +0200

    Added DOM relates #?

---
+++ Public/index.js
@@ -0,0 +1,9 @@
+document.getElementById("search").addEventListener("change", search);
+
+function search() {
+    var x = document.getElementById("search");
+    x.value = x.value.toUpperCase();
+}
+
+
+//https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_datalist
