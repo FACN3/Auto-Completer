571e4741231fd586643720c3b86d4db83aa57b8c
SamiSha99 <sami.shakkour@hotmail.com>
2017-11-22 12:39:54 +0200

    Added XHR request realte #?

--- Public/index.js
+++ Public/index.js
@@ -1,9 +1,18 @@
 document.getElementById("search").addEventListener("change", search);
 
-function search() {
-    var x = document.getElementById("search");
-    x.value = x.value.toUpperCase();
-}
+function search(url, callback) {
+  var xhr = new XMLHttpRequest(url);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      callback(JSON.parse(xhr.responseText));
+    } else {
+      console.log(xhr.readyState, xhr.status);
+      return "Sorry, there was an error.";
+    }
+  };
 
+  xhr.open("GET", url, true);
+  xhr.send();
+}
 
 //https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_datalist
