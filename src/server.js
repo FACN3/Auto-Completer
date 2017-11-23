0b762228c906a3cba84d29380928780d1fef6e0f
MynahMarie <mynah.marie@gmail.com>
2017-11-23 10:57:24 +0200

    Try to fix error in deploy

--- src/server.js
+++ src/server.js
@@ -3,10 +3,10 @@ var http = require('http');
 var router = require('./router');
 
 var port = process.env.PORT || 4000;
-var host = process.env.HOST || 'localhost';
+var host = process.env.HOST || '127.0.0.1';
 
 var server = http.createServer(router);
 
 server.listen(port);
 
-console.log('Server is listening on PORT 4000...');
+console.log('Server is listening on PORT ' + port);
