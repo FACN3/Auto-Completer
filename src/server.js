0c4db29f13e262531ebce1ff60057b9d7c5d84e0
MynahMarie <mynah.marie@gmail.com>
2017-11-23 11:18:26 +0200

    Change display of results to dropdown
    Relates #44

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@
 var http = require('http');
 var router = require('./router');
 
-var port = 4000;
+var host = process.env.HOST || 'localhost';
+var port = process.env.PORT || 4000;
 
 var server = http.createServer(router);
 
 server.listen(port);
 
-console.log('Server is listening on PORT 4000...');
+console.log('Server is listening on PORT ' + port);
